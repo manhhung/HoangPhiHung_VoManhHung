0a1a26269b33d126d621c939072edff091412ae7
manhhung <vo@manhhung.cf>
2015-04-03 07:42:53 +0700

    Photo
    Photo

---
+++ Photo/Photo/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Photo
+{
+    partial class frMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbxPhoto = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuView = new System.Windows.Forms.ToolStripMenuItem();
+            this.ctxMenuphoto = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mnuImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuImageScale = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuImageStretch = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuImageActual = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.ctxMenuphoto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbxPhoto
+            // 
+            this.pbxPhoto.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbxPhoto.ContextMenuStrip = this.ctxMenuphoto;
+            this.pbxPhoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxPhoto.Location = new System.Drawing.Point(0, 24);
+            this.pbxPhoto.Name = "pbxPhoto";
+            this.pbxPhoto.Size = new System.Drawing.Size(369, 307);
+            this.pbxPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxPhoto.TabIndex = 1;
+            this.pbxPhoto.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuView});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(369, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuLoad,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuLoad
+            // 
+            this.mnuLoad.Name = "mnuLoad";
+            this.mnuLoad.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.mnuLoad.Size = new System.Drawing.Size(140, 22);
+            this.mnuLoad.Text = "&Load";
+            this.mnuLoad.Click += new System.EventHandler(this.mnuLoad_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(137, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(140, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // mnuView
+            // 
+            this.mnuView.DropDown = this.ctxMenuphoto;
+            this.mnuView.Name = "mnuView";
+            this.mnuView.Size = new System.Drawing.Size(44, 20);
+            this.mnuView.Text = "&View";
+            this.mnuView.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.ProcessPhoto);
+            // 
+            // ctxMenuphoto
+            // 
+            this.ctxMenuphoto.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuImage});
+            this.ctxMenuphoto.Name = "ctxMenuphoto";
+            this.ctxMenuphoto.Size = new System.Drawing.Size(108, 26);
+            // 
+            // mnuImage
+            // 
+            this.mnuImage.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuImageScale,
+            this.mnuImageStretch,
+            this.mnuImageActual});
+            this.mnuImage.Name = "mnuImage";
+            this.mnuImage.Size = new System.Drawing.Size(107, 22);
+            this.mnuImage.Text = "&Image";
+            this.mnuImage.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.ProcessPhoto);
+            // 
+            // mnuImageScale
+            // 
+            this.mnuImageScale.Checked = true;
+            this.mnuImageScale.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mnuImageScale.Name = "mnuImageScale";
+            this.mnuImageScale.Size = new System.Drawing.Size(152, 22);
+            this.mnuImageScale.Tag = "Zoom";
+            this.mnuImageScale.Text = "&Scale to Fit";
+            // 
+            // mnuImageStretch
+            // 
+            this.mnuImageStretch.Name = "mnuImageStretch";
+            this.mnuImageStretch.Size = new System.Drawing.Size(152, 22);
+            this.mnuImageStretch.Tag = "StretchImage";
+            this.mnuImageStretch.Text = "S&tretch to Fit";
+            // 
+            // mnuImageActual
+            // 
+            this.mnuImageActual.Name = "mnuImageActual";
+            this.mnuImageActual.Size = new System.Drawing.Size(152, 22);
+            this.mnuImageActual.Tag = "Normal";
+            this.mnuImageActual.Text = "&Actual Size";
+            // 
+            // frMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 331);
+            this.Controls.Add(this.pbxPhoto);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frMain";
+            this.Text = "My photos";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPhoto)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ctxMenuphoto.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxPhoto;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuLoad;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuView;
+        private System.Windows.Forms.ContextMenuStrip ctxMenuphoto;
+        private System.Windows.Forms.ToolStripMenuItem mnuImage;
+        private System.Windows.Forms.ToolStripMenuItem mnuImageScale;
+        private System.Windows.Forms.ToolStripMenuItem mnuImageStretch;
+        private System.Windows.Forms.ToolStripMenuItem mnuImageActual;
+    }
+}
+
---
+++ Photo/Photo/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Photo
+{
+    public partial class frMain : Form
+    {
+        public frMain()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void mnuLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open photo";
+            // mở định dạng các file
+            dlg.Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try //kiểm tra phần mở rộng
+                {
+                    pbxPhoto.Image = new Bitmap(dlg.OpenFile());
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show("Unable to load files: " + ex.Message);
+                    pbxPhoto.Image = null;
+                }
+            }
+
+
+        }
+
+        private void ProcessPhoto(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ToolStripItem item = e.ClickedItem;
+            string enumVal = item.Tag as string;
+            if (enumVal != null)
+            {
+                pbxPhoto.SizeMode = (PictureBoxSizeMode)
+                    Enum.Parse(typeof(PictureBoxSizeMode), enumVal);
+            }
+        }
+    }
+}
